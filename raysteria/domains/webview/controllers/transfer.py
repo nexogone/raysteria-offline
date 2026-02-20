512b8a3498942a6ebd10b80fec2238844eefcacb
NeXoGone <nexogone@gmail.com>
2026-02-21 00:36:05 +1000

    Remove unused code

--- raysteria/domains/webview/controllers/transfer.py
+++ raysteria/domains/webview/controllers/transfer.py
@@ -16,8 +16,6 @@ class TransfersController(Controller):
         description=localize("webview", "transfer_combine_exec_desc"),
     )
     async def combine_exec_(self, combine_uuid: UUID) -> Response:
-        # uniwebview://combine.1.29a6ad32-3113-417b-8ec7-a15b44b51660
-        # return render_template("templates/combine_exec.html")
         print("[DEBUG] Combine UUID:", combine_uuid)
         return not_implemented()
 
@@ -27,12 +25,6 @@ class TransfersController(Controller):
         description=localize("webview", "transfer_combine_start_desc"),
     )
     async def combine_start_(self, combine_uuid: UUID) -> Response:
-        # https://www.bandainamcoid.com/v2/oauth2/auth
-
-        # client_id=tales_of_origin_and
-        # redirect_uri=https://rays.toco.tales-ch.jp/game_server/webview/transfers/combine_exec/29a6ad32-3113-417b-8ec7-a15b44b51660
-        # scope=JpGroupAll
-        # return render_template("templates/combine_start.html")
         print("[DEBUG] Combine UUID:", combine_uuid)
         return not_implemented()
 
@@ -42,7 +34,6 @@ class TransfersController(Controller):
         description=localize("webview", "transfer_transfer_exec_desc"),
     )
     async def transfer_exec_(self, transfer_uuid: UUID) -> Response:
-        # return render_template("templates/transfer_exec.html")
         print("[DEBUG] Transfer UUID:", transfer_uuid)
         return not_implemented()
 
@@ -52,11 +43,5 @@ class TransfersController(Controller):
         description=localize("webview", "transfer_transfer_start_desc"),
     )
     async def transfer_start_(self, transfer_uuid: UUID) -> Response:
-        # https://www.bandainamcoid.com/v2/oauth2/auth
-
-        # client_id=tales_of_origin_and
-        # redirect_uri=https://rays.toco.tales-ch.jp/game_server/webview/transfers/transfer_exec/b99f4dcc-c77c-4000-bc6a-562071073466
-        # scope=JpGroupAll
-        # return render_template("templates/transfer_start.html")
         print("[DEBUG] Transfer UUID:", transfer_uuid)
         return not_implemented()
